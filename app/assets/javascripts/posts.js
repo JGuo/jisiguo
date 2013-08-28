ac5e45820bbadb5bd13fa2ca0e2f52850fc5496b
Jisi Guo <jisiguo@gmail.com>
2013-08-27 18:13:28 -0700

    blurb stuff

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,19 +1,19 @@
 $(function(){
 	console.log("ready!");
 
-	var text = ["I'm a Northwestern alum, Go 'Cats!"
+	var text = ["I'm a <a class=\"northwestern\" target=\"_blank\"href=\"http://espn.go.com/college-football/team/_/id/77/northwestern-wildcats\">Northwestern</a> alum, Go 'Cats!"
 	,"I love snowboarding, nothing beats a powder day."
-	,"I was the Head of Product at <a target=\"_blank\"href=\"http://www.ribbon.co\">Ribbon</a>."
+	,"I was the Head of Product at <a class=\"ribbon\" target=\"_blank\"href=\"http://www.ribbon.co\">Ribbon</a>."
 	,"I was raised in the Windy City."
-	,"You should follow me on <a target=\"_blank\"href=\"http://www.dribbble.com/jisiguo\">Dribbble</a>."
-	,"You should follow me on <a target=\"_blank\"href=\"http://www.twitter.com/jisiguo\">Twitter</a>."
+	,"You should follow me on <a class=\"dribbble\" target=\"_blank\"href=\"http://www.dribbble.com/jisiguo\">Dribbble</a>."
+	,"You should follow me on <a class=\"twitter\" target=\"_blank\"href=\"http://www.twitter.com/jisiguo\">Twitter</a>."
 	,"I will usually say yes to an adventure."
 	,"I studied design, engineering and economics."
 	,"I love to travel and explore the world."
 	,"Minimalism tends to guide my design decisions."
-	,"I created <a target=\"_blank\"href=\"http://www.builtinchicago.org/community/questions?title=&field_answer_count_value=&created=&last_updated=&sort_by=created&sort_order=DESC\">Chicago Founders</a>, a community for Chicago entrepreneurs."
+	,"I created <a target=\"_blank\"href=\"http://www.builtinchicago.org/community/questions?title=&field_answer_count_value=&created=&last_updated=&sort_by=created&sort_order=DESC\">Chicago Founders</a>, a community for entrepreneurs."
 	,"I've lived in Istanbul during two summers in college."
-	,"I've traveled to 17 countries around the world."
+	,"I've traveled to 18 countries around the world."
 	,"I can speak english, mandarin and spanish."
 	,"I love playing pickup basketball."
 	,"I'm an avid runner."
@@ -23,6 +23,7 @@ $(function(){
 	,"I have recently gotten into cycling."
 	,"I'm very interested in the theory of <a target=\"_blank\"href=\"http://en.wikipedia.org/wiki/Flow_(psychology)\">flow</a>.</a>"
 	,"My name means \"lucky\" in Chinese."
+	,"You should connect with me on <a class=\"linkedin\" target=\"_blank\"href=\"http://www.linkedin.com/in/jisiguo\">LinkedIn</a>."
 	]
 
 	function shuffle(o){ //v1.0
@@ -31,7 +32,7 @@ $(function(){
 	};
 
 	var list = [];
-	for (var i = 0; i <= 21; i++) {
+	for (var i = 0; i <= 22; i++) {
 	    list.push(i);
 	}
 
@@ -44,7 +45,7 @@ $(function(){
 		$("#about-me").html(text[x]);
 		console.log(x);
 		index++;
-		if(index>21) {
+		if(index>22) {
 			console.log("RESTART!")
 			index = 0;
 			shuffled = shuffle(list);
