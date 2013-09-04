e6133be41d1a6e7c54e3749247c07a457ee9213f
Jisi Guo <jisiguo@gmail.com>
2013-09-04 14:54:23 -0700

    tweet button

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -67,4 +67,21 @@ $(function(){
 
 	hoverListener();
 
+	$('.tweet').click(function(event) {
+	    var width  = 550,
+	        height = 390,
+	        left   = ($(window).width()  - width)  / 2,
+	        top    = ($(window).height() - height) / 2,
+	        url    = this.href,
+	        opts   = 'status=1' +
+	                 ',width='  + width  +
+	                 ',height=' + height +
+	                 ',top='    + top    +
+	                 ',left='   + left;
+
+	    window.open(url, 'twitter', opts);
+
+	    return false;
+	});
+
 });
