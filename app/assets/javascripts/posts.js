30c113a2535082934829b149fa78bafab7326605
Jisi Guo <jisiguo@gmail.com>
2013-09-04 13:54:59 -0700

    touched up about me hover

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -2,28 +2,26 @@ $(function(){
 	console.log("ready!");
 
 	var text = ["I'm a <a class=\"northwestern\" target=\"_blank\"href=\"http://espn.go.com/college-football/team/_/id/77/northwestern-wildcats\">Northwestern</a> alum, Go 'Cats!"
-	,"I love snowboarding, nothing beats a <a target=\"_blank\"href=\"http://www.onthesnow.com/lake-tahoe/skireport.html\">powder day</a>."
+	,"I'm always on the lookout for a <a target=\"_blank\"href=\"http://www.onthesnow.com/lake-tahoe/skireport.html\">powder day</a>."
 	,"I was the Head of Product at <a class=\"ribbon\" target=\"_blank\"href=\"http://www.ribbon.co\">Ribbon</a>."
 	,"I was raised in the Windy City."
 	,"You should follow me on <a class=\"dribbble\" target=\"_blank\"href=\"http://www.dribbble.com/jisiguo\">Dribbble</a>."
 	,"You should follow me on <a class=\"twitter\" target=\"_blank\"href=\"http://www.twitter.com/jisiguo\">Twitter</a>."
-	,"I can never turn down a good adventure."
-	,"I studied design, engineering and economics."
-	,"I love traveling and exploring the world."
+	,"I studied engineering, economics and design."
 	,"Minimalism tends to guide my design decisions."
-	,"I created <a target=\"_blank\"href=\"http://www.builtinchicago.org/community/questions?title=&field_answer_count_value=&created=&last_updated=&sort_by=created&sort_order=DESC\">Chicago Founders</a>, a community for entrepreneurs."
-	,"I've lived in Istanbul during two summers in college."
+	,"I created <a target=\"_blank\"href=\"http://www.builtinchicago.org/community/questions?title=&field_answer_count_value=&created=&last_updated=&sort_by=created&sort_order=DESC\">Chicago Founders</a>."
+	,"I've lived in Istanbul for two summers."
 	,"I've traveled to 18 countries around the world."
 	,"I can speak english, mandarin and spanish."
 	,"I love playing pickup basketball."
 	,"I'm an avid runner."
 	,"Empathy for the user leads to good design."
-	,"I'm interested in both software and hardware design."
+	,"I'm interested in software and hardware design."
 	,"I'm a product designer, specializing in UX."
-	,"I have recently gotten into cycling."
-	,"I'm very interested in the theory of <a target=\"_blank\"href=\"http://en.wikipedia.org/wiki/Flow_(psychology)\">flow</a>.</a>"
+	,"I'm interested in the theory of <a target=\"_blank\"href=\"http://en.wikipedia.org/wiki/Flow_(psychology)\">flow</a>.</a>"
 	,"My name means \"lucky\" in Chinese."
 	,"You should connect with me on <a class=\"linkedin\" target=\"_blank\"href=\"http://www.linkedin.com/in/jisiguo\">LinkedIn</a>."
+	,"I love the outdoors and adventure sports."
 	]
 
 	function shuffle(o){ //v1.0
@@ -32,7 +30,7 @@ $(function(){
 	};
 
 	var list = [];
-	for (var i = 0; i <= 22; i++) {
+	for (var i = 0; i <= 20; i++) {
 	    list.push(i);
 	}
 
@@ -45,7 +43,7 @@ $(function(){
 		$("#about-me").html(text[x]);
 		console.log(x);
 		index++;
-		if(index>22) {
+		if(index>20) {
 			console.log("RESTART!")
 			index = 0;
 			shuffled = shuffle(list);
